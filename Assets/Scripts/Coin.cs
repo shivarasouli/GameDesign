574dc7532cb2c0644885fbe370f86a20ff7eb8c8
Shiva Rasouli <121674624+shivarasouli@users.noreply.github.com>
2024-10-15 19:25:20 -0400

    Adding coin collection
    added coin collection and changed player, game manager scripts as well

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameManager.Instance.AddCoin();  // Fixed the reference to use 'Instance'
+            Destroy(gameObject);
+        }
+    }
+}
